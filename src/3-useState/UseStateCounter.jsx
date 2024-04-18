dc39faba5c38448ae3b3f03a141e0dff32fa32e5
Burakkturk <burak1845turk@icloud.com>
2024-04-18 12:25:59 +0300

    dec

--- src/3-useState/UseStateCounter.jsx
+++ src/3-useState/UseStateCounter.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react"
 
 
 const UseStateCounter = () => {
+    const [count, setCount] = useState(0)
+    const handleInc = () => {
+        setCount(count + 1) 
+        console.log(count) 
+
+    }
   return (
     <div>
         <div>
             <h2>USE STATE HOOK</h2>
-            <h1>Count:0</h1>
-            <button>INC</button>
-            <button>CLR</button>
-            <button>DEC</button>
+            <h1>Count:{count}</h1>
+            <button onClick={handleInc}>INC</button>
+            <button onDoubleClick={() => setCount(0)}>CLR</button>
+            <button onClick={() => count > 0 && setCount(count - 1)}>DEC</button>
+            
         </div>
     </div>
   )
